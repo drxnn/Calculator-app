7d10ff0dba1171092ebd459566c07c66ac3e17e8
Drin Bratotini <drin.bratotini@gmail.com>
2023-06-25 14:34:55 -0400

    added functions

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let percentage = (value) => {
   return value / 100;
 };
 
-// btn-click
+// btn-click // FUNCTIONS FOR BUTTONS //
 
 let selectOne = () => {
   let one = document.getElementById("numOne").textContent;
@@ -35,6 +35,59 @@ let selectTwo = () => {
   let two = document.getElementById("numTwo").textContent;
   let screen = document.getElementById("screen");
   screen.innerText += two;
+};
+let selectThree = () => {
+  let three = document.getElementById("numThree").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += three;
+};
+
+let selectFour = () => {
+  let four = document.getElementById("numFour").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += four;
+};
+
+let selectFive = () => {
+  let five = document.getElementById("numFive").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += five;
+};
+
+let selectSix = () => {
+  let six = document.getElementById("numSix").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += six;
+};
+
+let selectSeven = () => {
+  let seven = document.getElementById("numSeven").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += seven;
+};
+
+let selectEight = () => {
+  let eight = document.getElementById("numEight").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += eight;
+};
+
+let selectNine = () => {
+  let nine = document.getElementById("numNine").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += nine;
+};
+
+let selectZero = () => {
+  let zero = document.getElementById("numZero").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += zero;
+};
+
+let addDecimal = () => {
+  let decimal = document.getElementById("dec").textContent;
+  let screen = document.getElementById("screen");
+  screen.innerText += decimal;
 };
 
 let allClear = () => {
